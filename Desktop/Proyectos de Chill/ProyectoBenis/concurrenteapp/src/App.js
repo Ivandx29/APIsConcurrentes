9b0a17322c10593d4f90f35de6c4177b564e48e4
brisa <brisa12631@gmail.com>
2022-09-22 16:39:42 -0500

    estilos

--- Desktop/Proyectos de Chill/ProyectoBenis/concurrenteapp/src/App.js
+++ Desktop/Proyectos de Chill/ProyectoBenis/concurrenteapp/src/App.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
-import "primeicons/primeicons.css";                                //icons
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
-import PokemonService from './service/PokemonService';
-import UserService from './service/UserService';
-
+import React, { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+import PokemonService from "./service/PokemonService";
+import UserService from "./service/UserService";
+import "./App.css";
 
 const App = () => {
-
   const [pokemon, setPokemon] = useState([]);
   const [user, setUser] = useState([]);
 
@@ -22,46 +21,51 @@ const App = () => {
     pokemonService.pokemon().then((data) => {
       setPokemon(data.results);
     });
-  }
+  };
 
   const usuarios = () => {
     userService.usuarios().then((data) => {
       setUser(data);
-      console.log(user)
+      console.log(user);
     });
-  }
+  };
 
-  useEffect(() => {
-  }, []);
-
+  useEffect(() => {}, []);
 
   const footer = (
     <span>
-      <Button label="Consultar API's" icon="pi pi-check" onClick={() => {pokemones(); usuarios()}} />
+      <Button
+        label="Consultar API's"
+        icon="pi pi-check"
+        onClick={() => {
+          pokemones();
+          usuarios();
+        }}
+      />
     </span>
   );
 
-
   return (
     <div className="card">
-      <div className='Tablas'>
-        <Card title="API Pokemon" style={{ width: '15em' }} footer={footer} />
-        <DataTable value={pokemon} responsiveLayout="scroll">
-          <Column field="name" header="Nombre"></Column>
-          <Column field="url" header="URL"></Column>
-        </DataTable>
-      </div>
-      <h1>
-        Pinche Benis
-      </h1>
-      <div className="Tablas">
-        <DataTable value={user} responsiveLayout="scroll">
-          <Column field="first_name" header="Nombre"></Column>
-          <Column field="gender" header="Sexo"></Column>
-        </DataTable>
+      <Card title="API Pokemon" className="titulo" footer={footer} />
+      <div className="container">
+        <div className="tabla1">
+          <h1>Pokemon</h1>
+          <DataTable value={pokemon} responsiveLayout="scroll">
+            <Column field="name" header="Nombre"></Column>
+            <Column field="url" header="URL"></Column>
+          </DataTable>
+        </div>
+        <div className="tabla2">
+          <h1>Usuarios</h1>
+          <DataTable value={user} responsiveLayout="scroll">
+            <Column field="first_name" header="Nombre"></Column>
+            <Column field="gender" header="Sexo"></Column>
+          </DataTable>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
